a3e945fc7e24fc34b0bb0ec6327108758ac4cf1e
yuchiko <iyuchiko@gmail.com>
2017-06-22 23:24:56 +0300

    add preloader

--- dev/assets/js/include.js
+++ dev/assets/js/include.js
@@ -4,6 +4,7 @@
 	// on ready
 	$(function() {
 		showLettersAnimation();
+		PageLoad();
 	});
 
 	// local scope code
@@ -17,6 +18,17 @@
 				.from('.js-v', 1.6, {rotation: 130, transformOrigin: "140% 50%"},'-=0.8')
 				.from('.js-s', 1, {y: -400},'-=0.5')
 				.from('.logo__bi', 1, {opacity:0});
+	}
+
+	function PageLoad() {
+		setTimeout(function() {
+			$("body").removeClass("is-pagetransition");
+		}, 100);
+
+		setTimeout(function() {
+			$("body").addClass("is-pagetransitionend");
+			$('.loader-overlay').hide();
+		}, 1400);
 	}
 
 }(window.jQuery, window, document));
