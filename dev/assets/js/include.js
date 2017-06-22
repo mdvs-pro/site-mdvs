521489dfc33da1ed609c1fe0037365ee60e25212
yuchiko <iyuchiko@gmail.com>
2017-06-22 20:17:23 +0300

    add letters animation

--- dev/assets/js/include.js
+++ dev/assets/js/include.js
@@ -3,9 +3,20 @@
 
 	// on ready
 	$(function() {
-
+		showLettersAnimation();
 	});
 
 	// local scope code
+
+	function showLettersAnimation() {
+		var tl = new TimelineMax();
+
+			tl
+				.from('.js-m', 1.6, {scale: 0, opacity: 0})
+				.from('.js-d', 1, {scale: 0},'-=1')
+				.from('.js-v', 1.6, {rotation: 130, transformOrigin: "140% 50%"},'-=0.8')
+				.from('.js-s', 1, {y: -400},'-=0.5')
+				.from('.logo__bi', 1, {opacity:0});
+	}
 
 }(window.jQuery, window, document));
